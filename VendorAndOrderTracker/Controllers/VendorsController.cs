066a10a7f03f15d02c9bb210646d533be3a99b32
Thomas Russell <t.p.russell19@gmail.com>
2021-05-14 14:54:18 -0700

    adjust params to fit new constructor

--- VendorAndOrderTracker/Controllers/VendorsController.cs
+++ VendorAndOrderTracker/Controllers/VendorsController.cs
@@ -39,11 +39,11 @@ namespace VendorAndOrderTracker.Controllers
     }
 
     [HttpPost("/vendors/{vendorId}/orders")]
-    public ActionResult Create(int vendorId, string orderDescription)
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, int orderPrice, string orderDate)
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
       Vendor foundVendor = Vendor.Find(vendorId);
-      Order newOrder = new Order(orderDescription);
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
       foundVendor.AddOrder(newOrder);
       List<Order> vendorOrders = foundVendor.Orders;
       model.Add("orders", vendorOrders);

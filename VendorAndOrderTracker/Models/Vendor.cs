6ddd6f7725cc46fcdde33f544ae383ef75c3336b
Thomas Russell <t.p.russell19@gmail.com>
2021-05-15 13:55:13 -0700

    add passing code to find vendor by Id

--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -32,5 +32,10 @@ namespace VendorAndOrderTracker.Models
     {
       return _instances;
     }
+
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
   }
 }
